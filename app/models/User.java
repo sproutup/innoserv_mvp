0ea1736fd4f99c0450e5c37b9a77e8201b7129f5
nitinj <nitin@sproutup.co>
2014-12-16 19:52:09 -0500

    fixed error related to duplicate entry

--- app/models/User.java
+++ app/models/User.java
@@ -65,10 +65,7 @@ public class User extends Model implements Subject {
 
 	public Date dateofbirth;
 
-	public String gender;
-
-	public Date dateofbirth;
-
+	
 	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
 	public Date lastLogin;
 
