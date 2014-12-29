4d8ef4ef2ab527cc36aa3d9317cf2ad3e74a7180
Tao Ni <tao.ni.81@gmail.com>
2014-12-28 23:45:21 -0500

    product item page css

--- public/javascripts/qualtricsPopup.js
+++ public/javascripts/qualtricsPopup.js
@@ -9,7 +9,7 @@ $('a.modalButton').on('click', function(e) {
     var src = $(this).attr('data-src');
     var height = $(this).attr('data-height') || 300;
     var width = $(this).attr('data-width') || 400;
-    
+
     $("#myModal iframe").attr({'src':src,
                         'height': height,
                         'width': width});
