d107f818b2e89d5b1f26195c05778d660c059789
Tao Ni <tao.ni.81@gmail.com>
2015-02-18 16:26:48 -0500

    Merge

--- app/controllers/FeedbackManagement.java
+++ app/controllers/FeedbackManagement.java
@@ -10,7 +10,7 @@ import play.Routes;
 import play.data.Form;
 import play.mvc.*;
 import providers.MyUsernamePasswordAuthProvider;
-import utils.AppConstants;
+//import utils.AppConstants;
 import views.html.*;
 
 public class FeedbackManagement extends Controller {
@@ -25,27 +25,28 @@ public class FeedbackManagement extends Controller {
    */
   public static Result fetchPopupQSurvey(Long productID){
 
-	  //TODO add logic to fetch belleds survey url based on productID
-		String belleds_url = AppConstants.BELLEDS_URL_AS_STRING;
+//	  //TODO add logic to fetch belleds survey url based on productID
+//		String belleds_url = AppConstants.BELLEDS_URL_AS_STRING;
+//
+//		//TODO get user credentials from the session
+//		User user = Application.getLocalUser(session());
+//
+//		if (user == null) {
+//			Logger.debug(">>User is null");
+//			//go to login page
+//			//return ok(login.render(MyUsernamePasswordAuthProvider.LOGIN_FORM));
+//		}
+//
+//		String userEmail = user.email;
+//		String userName =user.name;
+//		Logger.debug("ready to redirect to Qualtrics");
+//
+//		belleds_url = belleds_url + "&userEmail=" + userEmail + "&userName=" + userName;
+//
+//
+//	  return redirect(belleds_url);
 
-		//TODO get user credentials from the session
-		User user = Application.getLocalUser(session());
-			
-		if (user == null) {
-			Logger.debug(">>User is null");
-			//go to login page
-			//return ok(login.render(MyUsernamePasswordAuthProvider.LOGIN_FORM));
-		}
-		
-		String userEmail = user.email;
-		String userName =user.name;
-		Logger.debug("ready to redirect to Qualtrics");
-		
-		belleds_url = belleds_url + "&userEmail=" + userEmail + "&userName=" + userName;
-	  
-	  
-	  return redirect(belleds_url); 
-	  
+      return ok();
 		
 	}
 
