8554e0977926709277c10a1b35c476eb9c128c5b
thomasbeta <tbobrien612@gmail.com>
2015-09-01 12:14:44 -0400

    protect against multiple saves + spinner on post button. fixed #391

--- app/assets/app/content/content.controller.js
+++ app/assets/app/content/content.controller.js
@@ -82,27 +82,45 @@ function ContentController($stateParams, $state, FeedService, AuthService, $root
         vm.selectedProduct = p.id;
     };
 
+    vm.postCount = 0;
     vm.addContent = function() {
-        if (!vm.selectedProduct) {
-            vm.productErrorMsg = true;
-        } else if (!vm.enteredBody) {
-            vm.productErrorMsg = false;
-            vm.textErrorMsg = true;
-        } else {
-            var Post = postService.post();
-            var item = new Post();
-            item.body = vm.enteredBody;
-            item.product_id = vm.selectedProduct;
-            item.$save(function(res) {
-                res.htmlBody = urlify(res.body);
-                vm.content.unshift(res);
-                vm.enteredBody = '';
-                vm.selectedProduct = null;
+        if (vm.postCount !== 1) {
+            if (!vm.selectedProduct) {
+                vm.productErrorMsg = true;
+            } else if (!vm.enteredBody) {
                 vm.productErrorMsg = false;
-                vm.textErrorMsg = false;
-            }, function(err) {
-                console.log(err);
-            });
+                vm.textErrorMsg = true;
+            } else {
+                var Post = postService.post();
+                var item = new Post();
+                vm.postCount = 1;
+                item.body = vm.enteredBody;
+                item.product_id = vm.selectedProduct;
+
+                var postButton = document.getElementsByClassName('content-post-button');
+                var spinner = new Spinner(opts).spin(postButton[0]);
+                setTimeout(function() {
+                    item.$save(function(res) {
+                        res.htmlBody = urlify(res.body);
+                        vm.content.unshift(res);
+                        vm.enteredBody = '';
+                        vm.selectedProduct = null;
+                        vm.productErrorMsg = false;
+                        vm.textErrorMsg = false;
+                        vm.postCount = 0;
+                        var spinnerToRemove = document.getElementsByClassName('spinner');
+                        spinnerToRemove[0].remove();
+                    }, function(err) {
+                        console.log(err);
+                        vm.postCount = 0;
+                        var spinnerToRemove = document.getElementsByClassName('spinner');
+                        spinnerToRemove[0].remove();
+                    });
+                }, 2000);
+
+            }
+        } else {
+            console.log('already posted');
         }
     };
 
@@ -119,6 +137,30 @@ function ContentController($stateParams, $state, FeedService, AuthService, $root
             return '<a href="' + url + '">' + displayedUrl + '</a>';
         });
     }
+
+    // logic for a spinner after the save—should be moved to a directive 
+    var opts = {
+          lines: 8, // The number of lines to draw
+          length: 16, // The length of each line
+          width: 23, // The line thickness
+          radius: 42, // The radius of the inner circle
+          scale: 0.13, // Scales overall size of the spinner
+          corners: 1, // Corner roundness (0..1)
+          color: 'white', // #rgb or #rrggbb or array of colors
+          opacity: 0.25, // Opacity of the lines
+          rotate: 0, // The rotation offset
+          direction: -1, // 1: clockwise, -1: counterclockwise
+          speed: 0.8, // Rounds per second
+          trail: 60, // Afterglow percentage
+          fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          className: 'spinner', // The CSS class to assign to the spinner
+          top: '50%', // Top position relative to parent
+          left: '50%', // Left position relative to parent
+          shadow: false,// Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          position: 'absolute' // Element positioning
+    };
 }
 
 })();
