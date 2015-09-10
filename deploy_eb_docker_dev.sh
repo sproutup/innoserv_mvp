6ab76cf752522c7a74334e53f4db07f748321ed1
Peter Andersen <peter@ryanandersen.dk>
2015-09-10 16:20:54 -0400

    removed label "latest" from deploy script

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -2,5 +2,5 @@ activator warn clean docker:stage
 cp conf/docker/master/Dockerfile target/docker/Dockerfile
 cd target/docker
 eb init -r us-west-2 -p docker -k endurance dev
-eb deploy -l latest
+eb deploy
 #eb create sproutup-dev -r us-west-2 -c sproutup-dev -t WebServer -i t2.small -s -k endurance
