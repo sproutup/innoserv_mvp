39621bc9a9a54fa5ea01eeaac397482e70a93594
nitinj <nitin@sproutup.co>
2015-09-18 08:12:12 -0400

    fixes #421

---
+++ app/controllers/ContestController.java
@@ -0,0 +1,157 @@
+package controllers;
+
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
+import be.objectify.deadbolt.java.actions.SubjectPresent;
+
+import com.avaje.ebean.Page;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import models.Contest;
+import models.Product;
+import models.User;
+import models.UserReferral;
+import play.libs.Json;
+import play.Logger;
+import play.Play;
+import play.data.Form;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import service.GoogleURLShortener;
+import views.html.admin.*;
+
+/**
+ * Manage Share Contest actions
+ */
+public class ContestController extends Controller {
+    
+	private static final Form<Contest> contestForm = Form.form(Contest.class);
+	private static final Boolean admin_enabled = Boolean.parseBoolean(Play.application().configuration().getString("admin.enabled"));
+
+	/*
+	 * Gets called when product detail page is loaded
+	 */
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result getActiveContest(Long productId) {//get contest information
+    	Map<String, Object> params = new HashMap<String, Object>();
+        params.put("product_id", productId);
+        params.put("active", "1");
+        //TODO nj: need to handle when there are more than one active contest? get the latest "order by id desc"? check if findUnique would fail..
+        Contest contest = Contest.find.where().allEq(params).findUnique();
+		ObjectNode rs;
+    	if (contest==null) {
+    		rs = Json.newObject();
+        	rs.put("active", false);
+    	} else {
+    		rs = contest.toJson();
+    	}
+    	return created(rs);
+    }
+    
+	/*
+	 * Gets called when user clicks on the SproutUp or Share button
+	 */
+	@SubjectPresent
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result getContestURL(String productSlug) {
+        User user = Application.getLocalUser(ctx().session());
+        if (user == null) {
+            return badRequest("User not found");
+        } else {
+        	
+        	ObjectNode rs = Json.newObject();
+    		String genURL = "http://sproutup.co/product/" + productSlug + "?refId=contest";
+            String shortURL= GoogleURLShortener.shortenURL(genURL);
+        	rs.put("url", shortURL);
+            return created(rs);
+        }
+    }
+    
+    /*
+     * This is post processing when user
+     * has shared the contest
+     */
+    @BodyParser.Of(BodyParser.Json.class)
+    @SubjectPresent
+    public static Result processShare() {
+        JsonNode json = request().body().asJson();
+        long contestId = json.findPath("contestId").longValue();
+
+        //update the counter
+        Contest cont = Contest.find.byId(contestId);
+
+            cont.totalNumParticipated++;
+            cont.update();
+	
+            return created(cont.toJson());
+            
+        }
+  
+    	
+    	/*
+    	 * for admin page
+    	 */
+    public static Result list() {
+        if(!admin_enabled){return notFound();};
+
+        Logger.debug("mode: " + play.api.Play.current().mode());
+        List<Contest> contests = Contest.findAll();
+        return ok(contest_list.render(contests));
+      }  
+    
+    public static Result newContest() {
+		  if(!admin_enabled){return notFound();};
+
+		  List<Product> products = new Product().getAllActive();
+		  Contest cont = new Contest();
+		  cont.active=true;
+		  cont.beginDate = Calendar.getInstance().getTime(); //Get the current date
+
+		  return ok(contest_management.render(contestForm.fill(cont), products));
+	  }
+    
+    public static Result details(Long id) {
+        if(!admin_enabled){return notFound();};
+        List<Product> products = new Product().getAllActive();
+        
+        return ok(contest_management.render(contestForm.fill(Contest.find.byId(id)), products));
+      }
+    
+    
+    public static Result save() {
+        if(!admin_enabled){return notFound();};
+
+        Form<Contest> contF = contestForm.bindFromRequest();
+        Contest contest = contF.get();
+
+        String[] postAction = request().body().asFormUrlEncoded().get("action");
+        if (postAction == null || postAction.length == 0) {
+          return badRequest("You must provide a valid action");
+        } else {
+          String action = postAction[0];
+          System.out.println("Action is: " + action);
+          if ("Copy".equals(action)) {
+        	  contest.id=null;
+        	  contest.product.id=null;
+           }
+        }  
+        if (contest.id == null) {
+        	contest.save();
+        } else {
+        	//update campaign
+        	contest.update();
+        }
+        
+       return redirect(routes.ContestController.list());
+    }
+    
+}
