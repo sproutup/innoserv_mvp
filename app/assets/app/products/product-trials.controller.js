24d0ccb6a4416d69d77d196d59927a753b0ee606
thomasbeta <tbobrien612@gmail.com>
2015-10-01 10:58:11 -0400

    Refactored product trials controller

--- app/assets/app/products/product-trials.controller.js
+++ app/assets/app/products/product-trials.controller.js
@@ -15,20 +15,16 @@ function ProductTrialsController(TrialService, $stateParams) {
     vm.trials = TrialService.productTrials().query({
         slug: $stateParams.slug
     }, function() {
+        vm.next = vm.trials.filter(function (item) {
+            return item.status === 1 || item.status === 2;
+        });
+        vm.active = vm.trials.filter(function (item) {
+            return item.status === 3;
+        });
+        vm.finished = vm.trials.filter(function (item) {
+            return item.status > 3;
+        });
         vm.trialInit = true;
-        // Divide trials into active, next, and finished
-        for (t = 0; t < vm.trials.length; t++) {
-            if (vm.trials[t].status === 1 || vm.trials[t].status === 2) {
-                vm.next.push(vm.trials[t]);
-            } else if (vm.trials[t].status === 3) {
-				vm.active.push(vm.trials[t]);
-			} else if (vm.trials[t].status > 3) {
-				vm.finished.push(vm.trials[t]);
-			}
-        }
-        // console.log(vm.active);
-        // console.log(vm.next);
-        // console.log(vm.finished);
     });
 }
 
