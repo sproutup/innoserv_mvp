7601707d191fca29a220952eb16e15d23bc40c86
Tao Ni <tao.ni.81@gmail.com>
2015-11-02 16:49:34 -0500

    bug fix dev deploy

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -2,7 +2,7 @@ activator warn clean docker:stage
 cp -R conf/docker/master/. target/docker/
 cd target/docker
 eb init -r us-west-2 -p docker -k endurance develop
-eb deploy 
+eb deploy develop
 #eb create test -r us-west-2 -c test -t WebServer -i t2.small -s -k endurance
 #eb create develop -r us-west-2 -c develop-sproutup-co -t WebServer -i t2.small -s -k endurance
 
