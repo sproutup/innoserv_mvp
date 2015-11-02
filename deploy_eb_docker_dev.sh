8603d396342227b1e3d89b3f6c9c33c81c13afa0
Tao Ni <tao.ni.81@gmail.com>
2015-11-02 15:12:05 -0500

    error fix in deploy

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -1,6 +1,6 @@
 activator warn clean docker:stage
 cp -R conf/docker/master/. target/docker/
 cd target/docker
-eb init -r us-west-2 -p docker -k endurance sproutup
+eb init -r us-west-2 -p docker -k endurance dev
 eb deploy
 #eb create test -r us-west-2 -c test -t WebServer -i t2.small -s -k endurance
