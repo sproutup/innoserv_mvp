1f14a8e261f21b14013c445c1ea20a4827346cfa
Tao Ni <tao.ni.81@gmail.com>
2015-11-02 14:48:45 -0500

    redeploy test site

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -3,4 +3,4 @@ cp -R conf/docker/master/. target/docker/
 cd target/docker
 eb init -r us-west-2 -p docker -k endurance sproutup
 #eb deploy
-eb create develop -r us-west-2 -c develop -t WebServer -i t2.small -s -k endurance
+eb create test -r us-west-2 -c test -t WebServer -i t2.small -s -k endurance
