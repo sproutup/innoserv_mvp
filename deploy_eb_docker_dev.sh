190aa5b8d17213da287fdec431d3601f8057a498
Tao Ni <tao.ni.81@gmail.com>
2015-11-02 14:42:17 -0500

    redeploy script

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -3,4 +3,4 @@ cp -R conf/docker/master/. target/docker/
 cd target/docker
 eb init -r us-west-2 -p docker -k endurance sproutup
 #eb deploy
-eb create dev -r us-west-2 -c dev -t WebServer -i t2.small -s -k endurance
+eb create develop -r us-west-2 -c develop -t WebServer -i t2.small -s -k endurance
