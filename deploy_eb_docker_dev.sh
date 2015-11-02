3cf2f15d4ac4971eb8db6b4959f09828202dec90
Tao Ni <tao.ni.81@gmail.com>
2015-11-02 15:57:42 -0500

    dev redeploy

--- deploy_eb_docker_dev.sh
+++ deploy_eb_docker_dev.sh
@@ -1,6 +1,8 @@
 activator warn clean docker:stage
 cp -R conf/docker/master/. target/docker/
 cd target/docker
-eb init -r us-west-2 -p docker -k endurance dev
-eb deploy
+eb init -r us-west-2 -p docker -k endurance develop
+eb deploy 
 #eb create test -r us-west-2 -c test -t WebServer -i t2.small -s -k endurance
+#eb create develop -r us-west-2 -c develop-sproutup-co -t WebServer -i t2.small -s -k endurance
+
