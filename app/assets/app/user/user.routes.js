371dd4d768226108af2061a47f2af083686860af
Thomas O'Brien <tbobrien612@gmail.com>
2015-11-18 11:28:00 -0500

    IN-125 IN-127 IN-124 The style for the forgot password flow on mvp site now matches the rest of the site.

--- app/assets/app/user/user.routes.js
+++ app/assets/app/user/user.routes.js
@@ -4,25 +4,25 @@ angular
 
 function config($stateProvider) {
     $stateProvider
-      .state('password', {
+      .state('user.password', {
         abstract: true,
         url: '/password',
         template: '<ui-view/>'
       })
-      .state('password.forgot', {
+      .state('user.password.forgot', {
         url: '/forgot',
         templateUrl: 'assets/app/user/password/forgot-password.client.view.html'
       })
-      .state('password.sent', {
+      .state('user.password.sent', {
         url: '/sent',
         templateUrl: 'assets/app/user/password/forgot-password-confirmation.client.view.html'
       })
-      .state('password.reset', {
+      .state('user.password.reset', {
         abstract: true,
         url: '/reset',
         template: '<ui-view/>'
       })
-      .state('password.reset.form', {
+      .state('user.password.reset.form', {
         url: '/:token',
         templateUrl: 'assets/app/user/password/reset-password.client.view.html'
       });
