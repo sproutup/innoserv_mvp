8e30947fab0bcab1b80dd4a740637186c2039367
Thomas O'Brien <tbobrien612@gmail.com>
2015-12-12 17:18:41 -0500

    Suggest product work. Comment and like directive hooked up but need clean up.

--- app/assets/app/suggest/suggest.controller.js
+++ app/assets/app/suggest/suggest.controller.js
@@ -8,11 +8,46 @@ angular
 // This controller contains logic for main buzz page as well as individual product buzz pages
 // Functions loadInit and loadMore have seperate queries for when a slug is present (product buzz page) vs. when there is no slug (main buzz page)
 
-SuggestController.$inject = ['$stateParams', '$state', 'SuggestService'];
+SuggestController.$inject = ['$stateParams', '$state', 'SuggestService', 'AuthService'];
 
-function SuggestController($stateParams, $state, SuggestService) {
+function SuggestController($stateParams, $state, SuggestService, AuthService) {
+	var vm = this;
+	vm.products = SuggestService.suggestedProducts();
+	vm.suggestProduct = suggestProduct;
 
-	console.log(SuggestService.suggestedProducts());
+	function activate() {
+        if(!AuthService.ready()){
+            var unbindWatch = $rootScope.$watch(AuthService.ready, function (value) {
+                if ( value === true ) {
+                  unbindWatch();
+                  activate();
+                }
+            });
+        }
+        else {
+            init();
+        }
+    }
+
+    function init() {
+        vm.user = angular.copy(AuthService.m.user);
+        if (vm.user.id) {
+            vm.myTrialProducts = MyTrialProductsService.query();
+        }
+    }
+
+	function suggestProduct() {
+		var suggestion = {
+			product: {
+				name: vm.suggestProductName,
+				url: vm.suggestedProductUrl
+			},
+			user: AuthService.m.user
+		};
+
+		vm.products.push(suggestion);
+
+	}
 
 }
 
--- app/assets/app/suggest/suggest.routes.js
+++ app/assets/app/suggest/suggest.routes.js
@@ -6,6 +6,15 @@ function config($stateProvider) {
     $stateProvider
         .state('user.suggest' ,{
             url: '/suggest',
+            templateUrl: 'assets/app/suggest/suggest.html',
+            controller: 'SuggestController',
+            controllerAs: 'vm',
+            data: {
+                title: 'Suggest Cool Products - SproutUp'
+            }
+        })
+        .state('user.suggestMock' ,{
+            url: '/suggestMock',
             templateUrl: 'assets/app/suggest/mock.suggest.html',
             controller: 'SuggestController',
             controllerAs: 'vm',
