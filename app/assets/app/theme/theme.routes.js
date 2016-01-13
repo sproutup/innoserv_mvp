25a20b4a0bb1133becfd7991801f7e535a41f129
Tao Ni <tao.ni.81@gmail.com>
2016-01-13 17:53:33 -0500

    browse campaigns mock - 1st checkin http://localhost:9000/theme/mocksbrowse-campaigns

--- app/assets/app/theme/theme.routes.js
+++ app/assets/app/theme/theme.routes.js
@@ -26,6 +26,10 @@ function config($stateProvider) {
         })
         .state('user.theme.mocks.hangout', {
           url: '/hangout',
-          templateUrl: 'assets/app/theme/mocks/hangout.html',
+          templateUrl: 'assets/app/theme/mocks/hangout.html'
+        })
+        .state('user.theme.mocks.browse-campaigns', {
+          url: 'browse-campaigns',
+          templateUrl: 'assets/app/theme/mocks/browse-campaigns.html'
         });
 }
