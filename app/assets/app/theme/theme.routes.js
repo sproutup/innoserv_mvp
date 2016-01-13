af9d132ae8b266b190791fefe79cdcfee41df820
Tao Ni <tao.ni.81@gmail.com>
2016-01-12 19:00:11 -0500

    scaffold routes and pages for mockups

---
+++ app/assets/app/theme/theme.routes.js
@@ -0,0 +1,31 @@
+angular
+    .module('sproutupApp')
+    .config(config);
+
+function config($stateProvider) {
+    $stateProvider
+        .state('user.theme', {
+          url: '/theme',
+          abstract: true,
+          template: '<ui-view/>',
+          controller: '',
+          controllerAs: ''
+        })
+        .state('user.theme.index' ,{
+            url: '',
+            templateUrl: 'assets/app/theme/theme-index.html'
+        })
+        .state('user.theme.components', {
+          url: '/components',
+          templateUrl: 'assets/app/theme/components.html',
+        })
+        .state('user.theme.mocks', {
+          url: '/mocks',
+          abstract: true,
+          template: '<ui-view/>'
+        })
+        .state('user.theme.mocks.hangout', {
+          url: '/hangout',
+          templateUrl: 'assets/app/theme/mocks/hangout.html',
+        });
+}
