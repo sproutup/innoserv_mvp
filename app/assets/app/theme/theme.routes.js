151d6e959668380eb1ab75176337abeb72186c7f
Tao Ni <tao.ni.81@gmail.com>
2016-02-08 17:29:03 -0500

    [mock update] buzz page sidebar displaying featured activities/hangouts and call for actions

--- app/assets/app/theme/theme.routes.js
+++ app/assets/app/theme/theme.routes.js
@@ -20,6 +20,10 @@ function config($stateProvider) {
           abstract: true,
           template: '<ui-view/>'
         })
+        .state('user.theme.mocks.landing', {
+          url: '/landing',
+          templateUrl: 'assets/app/theme/mocks/landing.html'
+        })
         .state('user.theme.mocks.hangout', {
           url: '/hangout',
           templateUrl: 'assets/app/theme/mocks/hangout.html'
