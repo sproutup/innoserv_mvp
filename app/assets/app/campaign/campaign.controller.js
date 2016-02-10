b159db8aa71777e6697ca4d271b49789abb5ab89
Thomas O'Brien <tbobrien612@gmail.com>
2016-02-10 16:21:47 -0500

    IN-489 #comment My stuff details

--- app/assets/app/campaign/campaign.controller.js
+++ app/assets/app/campaign/campaign.controller.js
@@ -5,9 +5,9 @@ angular
   .module('sproutupApp')
   .controller('CampaignController', CampaignController);
 
-CampaignController.$inject = ['CampaignService', '$state', 'AuthService'];
+CampaignController.$inject = ['CampaignService', '$state', 'AuthService', '$scope'];
 
-function CampaignController(CampaignService, $state, AuthService) {
+function CampaignController(CampaignService, $state, AuthService, $scope) {
   var vm = this;
   vm.product = {};
   vm.find = find;
@@ -25,14 +25,40 @@ function CampaignController(CampaignService, $state, AuthService) {
   }
 
   function findMyCampaigns() {
+    if (!AuthService.ready()) {
+      var listener = $scope.$watch(AuthService.ready, function(val) {
+        if(val) {
+          listener();
+          findMyCampaigns();
+        }
+      });
+      return;
+    }
+
     CampaignService.listMyContributions().query({
       userId: AuthService.m.user.id
-    }).$promise.then(function(data) {
+    }, function(data) {
       vm.myCampaigns = data;
-    },
-    function(error) {
+      for (var i = 0; i < vm.myCampaigns.length; i++) {
+        sortLog(vm.myCampaigns[i]);
+      }
+    }, function(error) {
       console.log(error);
     });
+  }
+
+  function sortLog(campaign) {
+    if (campaign.log) {
+      // Find approved logs
+      campaign.approved = campaign.log.filter(function(item) {
+        return item.state === 1;
+      });
+
+      // Find completed logs
+      campaign.completed = campaign.log.filter(function(item) {
+        return item.state === 10 || item.state === '10';
+      });
+    }
   }
 
   function findOne(campaignId) {
